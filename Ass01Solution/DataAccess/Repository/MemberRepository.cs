75e2d182190f992092c3ca9fd13a83821a780744
jhonny cuise <lophochaidang1990@gmail.com>
2022-06-26 17:44:52 +0700

    1st

---
+++ Ass01Solution/DataAccess/Repository/MemberRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess;
+using BusinessObject;
+
+namespace DataAccess.Repository
+{
+    public class MemberRepository : IMemberRepository
+    {
+        public void DeleteMember(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Member> GetMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member GetNameByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMember(Member member)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
