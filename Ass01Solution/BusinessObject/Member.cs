b1fdb7ed0616987e610e05ee56f63af56519148b
jhonny cuise <lophochaidang1990@gmail.com>
2022-06-27 09:45:12 +0700

    2nd

--- Ass01Solution/BusinessObject/Member.cs
+++ Ass01Solution/BusinessObject/Member.cs
@@ -3,10 +3,10 @@
     public class Member
     {
         public int MemberID { get; set; }
-        public string MemberName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
+        public string? MemberName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
     }
 }
