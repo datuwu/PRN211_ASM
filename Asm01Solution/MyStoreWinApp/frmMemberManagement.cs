2484b4c11ece8da34538c13c6081fb442eac33c9
datuwu <doixuihen@gmail.com>
2022-06-27 11:23:01 +0700

    design winform

---
+++ Asm01Solution/MyStoreWinApp/frmMemberManagement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyStoreWinApp
+{
+    public partial class frmMemberManagement : Form
+    {
+        public frmMemberManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtWelcomeUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
