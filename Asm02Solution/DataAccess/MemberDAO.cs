9ca0844cc38fc1a9aec12aa1b4101a28c289e861
lcht2701 <lecanhhoangthanh@gmail.com>
2022-06-30 09:11:30 +0700

    update BusinessObject ASM2

---
+++ Asm02Solution/DataAccess/MemberDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess;
+public class MemberDAO
+{
+    //Using Singleton Pattern
+    private static MemberDAO instance = null;
+    private static readonly object instanceLock = new object();
+    private MemberDAO() { }
+    public static MemberDAO Instance
+    {
+        get
+        {
+            lock (instanceLock)
+            {
+                if (instance == null)
+                {
+                    instance = new MemberDAO();
+                }
+                return instance;
+            }
+        }
+    }
+}
